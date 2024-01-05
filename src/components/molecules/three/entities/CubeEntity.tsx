f52da63df67dfde4af3a5457aa4a3a0f6d225b85
nikitak <nikitak@shedevre.com>
2024-01-05 17:30:45 +0200

    zxc

--- src/components/molecules/three/entities/CubeEntity.tsx
+++ src/components/molecules/three/entities/CubeEntity.tsx
@@ -1,20 +1,38 @@
 import { useFrame, useThree } from "@react-three/fiber";
+import { easeInOut, useMotionValue, useMotionValueEvent } from "framer-motion";
+import { motion } from "framer-motion-3d";
 import { useEffect, useRef, useState } from "react";
 
 const CubeEntity = () => {
-    const meshRef = useRef<any>();
-    const [rot, setRot] = useState(0);
 
-    const handleClick = (event: any) => {
-        console.log(event);
-        meshRef.current.rotation.x += 10;
+    const meshRef = useRef<any>(null);
+    useFrame((state, delta) => meshRef.current.rotation.y +=delta);
+
+    const [cubeOpacity, setCubeOpacity] = useState<number>(0);
+
+    const opacity = useMotionValue(0);
+
+    const handleClick = () => {
+        setCubeOpacity(1);
     }
 
+    useMotionValueEvent(opacity, "animationStart", () => {
+        console.log("animation started on opacity")
+      })
+      
+      useMotionValueEvent(opacity, "change", (latest) => {
+        console.log("opacity changed to", latest)
+      })
+
+      useEffect(() => {
+        opacity.get();
+      }, [cubeOpacity])
+
     return (
-        <mesh ref={meshRef} onClick={handleClick}>
-            <boxGeometry />
+        <motion.mesh position={[0, 0, 0]} ref={meshRef} onClick={handleClick}>
+            <motion.boxGeometry args={[1, 1, 1]} animate={{opacity: cubeOpacity}} />
             <meshStandardMaterial />
-        </mesh>
+        </motion.mesh>
     );
 }
 

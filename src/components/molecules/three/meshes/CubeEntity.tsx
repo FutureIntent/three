0811cb4130fee7574465e6b60c5564ff22d5f972
nikitak <nikitak@shedevre.com>
2024-02-01 17:29:57 +0200

    texture and skybox

--- src/components/molecules/three/meshes/CubeEntity.tsx
+++ src/components/molecules/three/meshes/CubeEntity.tsx
@@ -1,4 +1,4 @@
-import { useCubeTexture, useTexture } from "@react-three/drei";
+import { useTexture } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { animate, useMotionValue, useTransform } from "framer-motion";
 import { motion } from "framer-motion-3d";
@@ -6,8 +6,7 @@ import { useRef } from "react";
 
 const CubeEntity = () => {
 
-  const [cube, cube2] = useTexture(['textures/cubemap/bg.jpg', 'textures/cubemap/cube.png']);
-  const envMap = useCubeTexture(['bg.jpg', 'cube.png', 'bg.jpg', 'bg.jpg', 'bg.jpg', 'bg.jpg'], { path: 'textures/cubemap/' });
+  const [cube] = useTexture(['textures/cube/bg.jpg']);
 
   const cubeRef = useRef<any>(null);
   useFrame((state, delta) => cubeRef.current.rotation.y += delta);
@@ -19,7 +18,6 @@ const CubeEntity = () => {
 
   const handleClick = () => {
     console.log('click');
-    console.log(envMap);
   }
 
   const handlePointerOver = () => {
@@ -35,7 +33,12 @@ const CubeEntity = () => {
   return (
     <motion.mesh castShadow={true} position={[0, y, 4.5]} ref={cubeRef} onClick={handleClick} initial={{ x: -10 }} animate={{ x: 0, transition: { delay: 0.5, duration: 1, type: 'spring' } }} onPointerOver={handlePointerOver} onPointerOut={handlePointerOut} scale={scale}>
       <motion.boxGeometry args={[0.7, 1.1, 0.05]} />
-      <motion.meshStandardMaterial color={color} metalness={1} roughness={0.7} envMap={envMap} />
+      <motion.meshStandardMaterial attach="material-0" color={color} metalness={1} roughness={0.7} />
+      <motion.meshStandardMaterial attach="material-1" color={color} metalness={1} roughness={0.7} /> 
+      <motion.meshStandardMaterial attach="material-2" color={color} metalness={1} roughness={0.7} /> 
+      <motion.meshStandardMaterial attach="material-3" color={color} metalness={1} roughness={0.7} /> 
+      <motion.meshStandardMaterial attach="material-4" color={color} metalness={1} roughness={0.7} />
+      <motion.meshStandardMaterial attach="material-5" color={color} metalness={1} roughness={0.7} map={cube} />  
     </motion.mesh>
   );
 }
--- src/components/organisms/three/Three2.tsx
+++ src/components/organisms/three/Three2.tsx
@@ -5,6 +5,7 @@ import CubeEntity from "@components/molecules/three/meshes/CubeEntity";
 import { animate, useMotionValue, useMotionValueEvent } from "framer-motion";
 import { motion } from "framer-motion-3d";
 import { ThreeContext } from "@contexts/three/threeContext";
+import { Environment } from "@react-three/drei";
 
 const Three2 = () => {
 
@@ -12,11 +13,11 @@ const Three2 = () => {
 
     const threeHook = useThree();
 
-    const {three} = useContext(ThreeContext);
+    const { three } = useContext(ThreeContext);
 
     useEffect(() => {
         animate(z, [0, three.camera.position.z], { duration: 1, ease: 'easeOut' });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     useMotionValueEvent(z, "change", (latest) => {
@@ -25,6 +26,12 @@ const Three2 = () => {
 
     return (
         <>
+            <Environment
+                background={true}
+                blur={0} // blur factor between 0 and 1 (default: 0, only works with three 0.146 and up)
+                files={['corona_ft.png', 'corona_bk.png', 'corona_up.png', 'corona_dn.png', 'corona_lf.png', 'corona_rt.png']}
+                path="textures/skybox/"
+            />
             <color args={["#000000"]} attach="background" />
             <motion.spotLight color={'#DAA520'} position={[0.1, 2, 6]} intensity={50} castShadow={true} penumbra={1} />
             <PlaneEntity />
